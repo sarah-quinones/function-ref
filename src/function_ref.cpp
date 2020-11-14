c8597a39c5a7e87bc0498060ea76c748c51c7893
kitegi <>
2020-11-14 21:51:31 +0100

    error message

--- src/function_ref.cpp
+++ src/function_ref.cpp
@@ -1,9 +1,11 @@
 #include "function_ref.hpp"
 #include <exception>
+#include <cstdio>
 
 namespace veg {
 
 [[noreturn]] void terminate() {
+  std::fputs("function_ref called in null state", stderr);
   std::terminate();
 }
 
